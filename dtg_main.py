a669d6cf989369e5f5dc0faef1b179248e9ed482
Stephanie Caballero <stephcaballero@yahoo.com>
2022-08-12 13:32:42 -0500

    Added meal and transportation randoms

--- dtg_main.py
+++ dtg_main.py
@@ -1,14 +1,20 @@
+import random
+
 # As a developer, I want to make at least three commits with descriptive messages.
 
 # As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment selections in their own separate lists.
-destination_space_list = ["Tomorrowland," "Zagama Beach Saturn", "Olduvai Ruins Mars"]
-destination_sea_list = ["Atlanis Underwater Theme Park," "Carnival Day Cruise," "Sea World"]
 
-restaurant_ordinary_list = ["Big Boy," "Carls Junior", "Pei Wei"]
-restaurant_exotic_list = ["The Pearl Imporium," "Fishmongers," "Burger International"]
+destination_space_list = ["Tomorrowland", "Zagama Beach Saturn", "Olduvai Ruins Mars"]
+destination_sea_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", "Sea World"]
 
-entertainment_space_list = ["Jet Pack rentals," "Hanging pools," "All ride daypass," "Trackless train tours"]
-entertainment_sea_list = ["mini sub tours," "All day ride pass," " Botanical gardens," "Trench Tours"]
+restaurant_ordinary_list = ["Big Boy", "Carls Junior", "Pei Wei"]
+restaurant_exotic_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
+
+transportation_space_list = ["SpaceXMercedes Rocket", "Space Shuttle", "Ark Stargate"]
+transportation_water_list = ["MagLev Bullet train", "Vegas Submarines", "Torpedo Cruises"]
+
+entertainment_space_list = ["Jet Pack rentals", "Hanging pools", "All ride daypass", "Trackless train sky tours"]
+entertainment_sea_list = ["mini sub tours", "All day ride pass", " Botanical gardens", "Trench Tours"]
 
 # As a user, I want a destination to be randomly selected for my day trip.
 from multiprocessing.context import SpawnContext
@@ -20,41 +26,47 @@ print('Hello ' + your_name + ' Welcome to the Total Recall Live Action ' + 'Them
     
 day_trip_start = input("Type space or sea to begin your daytrip! ")
 
-destination_space_list = ["Tomorrowland," "Zagama Beach Saturn," "Olduvai Ruins Mars"]
-detination_water_list = ["Atlanis Underwater Theme Park," "Carnival Day Cruise," "Sea World"]
+destination_space_list = ["Tomorrowland", "Zagama Beach Saturn", "Olduvai Ruins Mars"]
+detination_water_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", "Sea World"]
 
 if day_trip_start =="space":
-    import random
     print(random.choice(destination_space_list))
 
 elif day_trip_start !="space":
-    import random
     print(random.choice(destination_sea_list))
 
 # As a user, I want a restaurant to randomly selected. 
-restaurant_ordinary_list = ["Big Boy," "Carls Junior", "Pei Wei"]
-restaurant_exotic_list = ["The Pearl Imporium," "Fishmongers," "Burger International"]
+restaurant_ordinary_list = ["Big Boy", "Carls Junior", "Pei Wei"]
+restaurant_exotic_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
 
 bk_lunch = input("Select a meal plan. Do you feel like the ordinary or exotic?: ")
 
 if bk_lunch == "ordinary":
-    import random
-    print(random.choice(restaurant_ordinary_list))
-
+    print(random.choice(restaurant_ordinary_list)+ " Excellent choice! Let's select your mode of transportation.")
+  
 elif bk_lunch != "ordinary":
-    import random
-    print(random.choice(restaurant_exotic_list))
+    print(random.choice(restaurant_exotic_list) + " Excellent choice! Let's select your mode of transportation.")
 
 
 # # As a user, I want a mode of transportation to be randomly selected for my day trip.
 
-# transportation_space_list = ["SpaceXMercedes Rocket," "Space Shuttle," "Ark Stargate"]
-# transportation_water_list = ["MagLev Bullet train," "Vegas Submarines," "Torpedo Cruises"]
+transportation_space_list = ["SpaceXMercedes Rocket", "Space Shuttle", "Ark Stargate"]
+transportation_water_list = ["MagLev Bullet train", "Vegas Submarines", "Torpedo Cruises"]
+
+if day_trip_trans == "day_trip_start":
+    print(random.choice(transportation_space_list) + "Fantastic! You'll be traveling in style! Next up, entertainment. ")
+
+elif day_trip_trans !="day_trip_start":
+    print(random.choice(transportation_water_list) + "Fantastic! You'll be traveling in style! Next up, entertainment. ")
 
 # # As a user, I want a form of entertainment to be randomly selected for my day trip.
 
-# entertainment_space_list = ["Jet Pack rentals," "Hanging pools," "All ride daypass," "Trackless train tours"]
-# entertainment_water_list = ["mini sub tours," "All day ride pass," " Botanical gardens," "Trench Tours"]
+entertainment_space_list = ["Jet Pack rentals," "Hanging pools," "All ride daypass," "Trackless train sky tours"]
+entertainment_water_list = ["mini sub tours," "All day ride pass," " Botanical gardens," "Trench Tours"]
+
+
+
+
 # # As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
 # day_trip_confirmation =  input(" Would you like to confirm revervation? Type Y for Yes / N for No")
 #     if day_trip_confirmation = Y
