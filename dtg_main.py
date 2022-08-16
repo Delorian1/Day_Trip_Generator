7931441e3ad64cdd1abf8cd964e5598294a50c6f
Stephanie Caballero <stephcaballero@yahoo.com>
2022-08-16 17:58:18 -0500

    added confirmations

--- dtg_main.py
+++ dtg_main.py
@@ -1,4 +1,6 @@
 import random
+from random import choice
+
 
 # As a developer, I want to make at least three commits with descriptive messages.
 
@@ -24,13 +26,15 @@ from tkinter.messagebox import YES
 your_name = input("Please enter your name: ")
 print('Hello ' + your_name + ' Welcome to the Total Recall Live Action ' + 'Theme Parks. Where would you like to go?')
     
-day_trip_start = input("Type space or sea to begin your daytrip! ")
+day_trip_start = input("Type space or sea to begin your daytrip! ").lower()
 
 destination_space_list = ["Tomorrowland", "Zagama Beach Saturn", "Olduvai Ruins Mars"]
 detination_water_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", "Sea World"]
 
 if day_trip_start =="space":
     print(random.choice(destination_space_list))
+
+# store choices as a variable
 
 elif day_trip_start !="space":
     print(random.choice(destination_sea_list))
@@ -39,10 +43,10 @@ elif day_trip_start !="space":
 restaurant_ordinary_list = ["Big Boy", "Carls Junior", "Pei Wei"]
 restaurant_exotic_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
 
-bk_lunch = input("Select a meal plan. Do you feel like the ordinary or exotic?: ")
+bk_lunch = input("Select a meal plan. Do you feel like the ordinary or exotic?: ").lower()
 
 if bk_lunch == "space":
-    print(random.choice(restaurant_ordinary_list)+ " Excellent choice! Let's select your mode of transportation.")
+    print(random.choice(restaurant_ordinary_list) + " Excellent choice! Let's select your mode of transportation.")
   
 elif bk_lunch != "space":
     print(random.choice(restaurant_exotic_list) + " Excellent choice! Let's select your mode of transportation.")
@@ -73,29 +77,35 @@ elif bk_lunch != "ordinary":
 
 # As a user, I want to be able to randomly re-select a destination, restaurant, mode of transportation, and/or form of entertainment if I don’t like one or more of those things.
 
-day_trip_confirmation = input("Would you like to book your all-inclusive daytrip? Type yes or no: ").lower()
-if day_trip_confirmation == "yes":
+play_again = input("Would you like to book your all-inclusive daytrip? Type yes or no: ").lower()
+if play_again == "yes":
     print("Great, let's finalize and lock-in your reservation.") 
 
-else:
-    print("Sorry to see that the choices were not to your liking." + "Let's see if we can pair you up with a trip of a lifetime." + "If at first you don't succeed, try try again.")
-    
+elif play_again != "yes":
+     print("Sorry to see that the choices were not to your liking. Do you wish to try again? Type yes or no.")
+
+
+def play_again(): 
+    play_again = input("Do you wish to try again? Type yes or no: ") 
+       
+if play_again == "yes":
+    return input("Please enter your name: ")
   
     
 #  As a user, I want to be able to confirm that my day trip is “complete” once I like all of the random selections.
 
-day_trip_confirmation_2 =  input(" Would you like to finalize your revervation? Type yes to confirm: ").lower()
+day_trip_confirmation=  input(" Would you like to finalize your revervation? Type yes to confirm: ").lower()
 
-if day_trip_confirmation_2 == "yes":
+if day_trip_confirmation == "yes":
     print(" Your reservation is complete. Please see your reservation details below." + "Thank you for choosing Total Recall Live Action Theme Parks.")
     
 # # # As a user, I want to display my completed trip in the console.
 
-def day_trip_confirmation_2 ():
-#     print('Great choice! You have selected the' + {destination_space_list} +
-#             'Included in your day trip is the' + {entertainment_space_list} +
-#                 'entertainment package. You will travel in style via' + {transportation_space_list}. + 
-#                     You will get a breakfast and lunch pass for one and dine at {}.')
+# def day_trip_confirmation():
+#     if day_trip_start == "space":
+#        print('Great choice! You have selected the' + random.choice().final_destination())
 
+#     # elif day_trip_start != "space":
+#     #      print('Great choice! You have selected the' + random.choice().final_destination_2())
 
-# As a developer, I want all of my functions to have a Single Responsibility. Rember each function should do just one thing!
+# # As a developer, I want all of my functions to have a Single Responsibility. Rember each function should do just one thing!
