1d5bebc6db9555d4664eeef10209579a47996eb7
Stephanie Caballero <stephcaballero@yahoo.com>
2022-08-18 22:05:57 -0500

    addition of chice recall functionality

--- dtg_main.py
+++ dtg_main.py
@@ -8,16 +8,16 @@ from random import choice
 # As a developer, I want to make at least three commits with descriptive messages.
 
 # As a developer, I want to store my destinations, restaurants, mode of transportation, and entertainment selections in their own separate lists.
+recall_1
 destination_space_list = ["Tomorrowland", "Zagama Beach Saturn", "Olduvai Ruins Mars"]
-destination_sea_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", "Sea World"]
-
 restaurant_space_list = ["Big Boy", "Carls Junior", "Pei Wei"]
-restaurant_sea_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
-
 transportation_space_list = ["SpaceXMercedes Rocket", "Omni Space Shuttle", "Ark Stargate"]
-transportation_sea_list = ["MagLev Bullet train", "Vegas Submarines", "Torpedo Cruises"]
-
 entertainment_space_list = ["Jet Pack rentals", "Hanging pools", "All ride daypass", "Trackless train sky tours"]
+
+recall_2
+destination_sea_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", "Sea World"]
+restaurant_sea_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
+transportation_sea_list = ["MagLev Bullet train", "Vegas Submarines", "Torpedo Cruises"]
 entertainment_sea_list = ["Mini sub tours", "All day ride pass", "Botanical gardens", "Trench Tours"]
 
 
@@ -38,36 +38,29 @@ destination_sea_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", 
 
 
 if day_trip_start =="space":
-    print(random.choice(destination_space_list))
+    print(random.choice(destination_space_list) + "You have chosen our most popular package, our Space Odessy Package.")
 
 # store choices as a variable
 
 elif day_trip_start !="space":
-    print(random.choice(destination_sea_list))
+    print(random.choice(destination_sea_list) + " You'll love our new adventure called 20 Leagues Under the Sea! ")
 
-day_trip_confirm = input("Would you like to continue with selection? Type yes or no: ").lower()
+day_trip_confirm = input("Would you like to continue with day trip selection? Type yes or no: ").lower()
 
 
 # As a user, I want a restaurant to randomly selected. 
 restaurant_space_list = ["Big Boy", "Carls Junior", "Pei Wei"]
 restaurant_sea_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
 
-bk_lunch = input("Would you like the space or sea menu? Type space or sea: ").lower()
+bk_lunch = input("Next up meals. Breakfast and lunchare included on all daytrip packages. Please type space or sea: ").lower()
 
-if bk_lunch == "space":
+if day_trip_start == "space":
     print(random.choice(restaurant_space_list) + " Excellent choice! Let's select your mode of transportation.")
   
-elif bk_lunch != "space":
+elif day_trip_start != "space":
     print(random.choice(restaurant_sea_list) + " Excellent choice! Let's select your mode of transportation.")
 
 bk_lunch_confirm = input("Would you like to continue with selection? Type yes or no: ").lower()
-
-# while bk_lunch_confirm == "yes"
-#     continue
-
-# elif bk_lunch_confirm == "yes"
- 
-
 
 # As a user, I want a mode of transportation to be randomly selected for my day trip.
 
@@ -80,7 +73,7 @@ if day_trip_start  == "space":
 elif day_trip_start != "space":
     print(random.choice(transportation_sea_list) + " Fantastic! You'll be traveling in style! Next up, entertainment. ")
 
-# transportation_confirmation = input("Would you like to continue with selection? Type yes or no: ").lower()
+transportation_confirmation = input("Would you like to continue with selection? Type yes or no: ").lower()
 
 # # As a user, I want a form of entertainment to be randomly selected for my day trip.
 
@@ -99,13 +92,7 @@ entertainment_confirmation =  input("Would you like to continue with selection? 
 
 day_trip_book = input("Would you like to book your all-inclusive daytrip? Type yes or no: ").lower()
 
-def calc:
-    return restart()
-def restart():
-    while True:
-        again =  input("Would you like to book your all-inclusive daytrip? Type yes or no: ")
-        if again not in {"yes", "no"}:
-            print ("Please enter valid option
+
 
 
 
@@ -118,28 +105,39 @@ day_trip_confirmation =  input("Would you like to finalize your revervation? Typ
 if day_trip_confirmation == "yes":
     print(" Your reservation is complete. Please see your reservation details below." + "Thank you for choosing Total Recall Live Action Theme Parks.")
 
+destination_space_list = ["Tomorrowland", "Zagama Beach Saturn", "Olduvai Ruins Mars"]
+restaurant_space_list = ["Big Boy", "Carls Junior", "Pei Wei"]
+transportation_space_list = ["SpaceXMercedes Rocket", "Omni Space Shuttle", "Ark Stargate"]
+entertainment_space_list = ["Jet Pack rentals", "Hanging pools", "All ride daypass", "Trackless train sky tours"]
+
+destination_sea_list = ["Atlanis Underwater Theme Park", "Carnival Day Cruise", "Sea World"]
+restaurant_sea_list = ["The Pearl Imporium", "Fishmongers", "Burger International"]
+transportation_sea_list = ["MagLev Bullet train", "Vegas Submarines", "Torpedo Cruises"]
+entertainment_sea_list = ["Mini sub tours", "All day ride pass", "Botanical gardens", "Trench Tours"]
+
 
 choice_destination_space_list = choice(destination_space_list)
-choice_destination_sea_list = choice(destination_sea_list)
-
 choice_restaurant_space_list = choice(restaurant_space_list)
-choice_restaurant_sea_list = choice(restaurant_sea_list)
-
-choice_transportation_space_list = choice(transportation_space_list)
-choice_transportation_sea_list = choice(transportation_sea_list)
-
+choice_restaurant_space_list = choice(restaurant_space_list)
 choice_entertainment_space_list = choice(entertainment_space_list)
+
+choice_destination_sea_list = choice(destination_sea_list)
+choice_restaurant_sea_list = choice(restaurant_sea_list)
+choice_transportation_sea_list = choice(transportation_sea_list)
 choice_entertainment_sea_list = choice(entertainment_sea_list)
 
-
-
-
-print("You have chosen "+ "Destination:" + choice(destination_space_list) or choice(destination_sea_list))
-print("You will dine in style at " + "Dining: " + choice(restaurant_ordinary_list) or choice(restaurant_exotic_list))
-print("You will picked up at the port of call and taken to your destination via " + "Transportation" + choice(transportation_space_list) or choice(transportation_water_list))
-print("Your all inclusive daytrip will include an entertainment package: " + "Entertanment" + choice(entertainment_space_list) or choice(entertainment_sea_list))
+print("You have chosen "+ "Destination:" + choice(destination_list))
+print("You will dine in style at " + "Dining: " + choice(restaurant_list))
+print("You will picked up at the port of call and taken to your destination via " + "Transportation" + choice(transportation_list))
+print("Your all inclusive daytrip will include an entertainment package: " + "Entertanment" + choice(entertainment_list))
 print("Thank you for booking your daytrip with Total Recall Live Action Theme Parks where we make your dreams a reality!")
 
+print ("Type Y to continue")
+
+c1 = choice(destination_list)
+c2 = choice(restaurant_list)
+c3 = choice(transportation_list)
+c4 = choice(entertainment_list)
 
 # As a user, I want to display my completed trip in the console.
 #save itenerary
@@ -152,4 +150,4 @@ print("Thank you for booking your daytrip with Total Recall Live Action Theme Pa
 # elif day_trip_start != "space":
 #      print('Great choice! You have selected the' + random.choice().final_destination_2())
 
-# As a developer, I want all of my functions to have a Single Responsibility.Remember each function should do just one thing
+# As a developer, I want all of my functions to have a Single Responsibility.Remember each function should do just one thing 
